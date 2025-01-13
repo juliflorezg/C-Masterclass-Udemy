106d78e33eaa01ee935456a6d250cc0f754799fd
Julian Florez <forezjuli08@hotmail.com>
2025-01-12 19:51:14 -0500

    implicitly typed variables

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,23 +1,11 @@
 ﻿// .dll -> DLL = dynamic link library
 // ctrl + R + R to rename a variable
 
-int a = 4;
-int b = 10;
-++a;
-++b;
+// implicitly typed variables
+var word = "lol";
+var num = 0;
 
-Console.WriteLine("Addition: " + (a + b));
-Console.WriteLine("Subtraction: " + (a - b));
-Console.WriteLine("Multiplication: " + a * b);
-Console.WriteLine("Division: " + a / b);
-Console.WriteLine("John" + " "+ "Smith");
-
-string word = "lol";
-Console.WriteLine(word + a);
-Console.WriteLine(a % b);
-Console.WriteLine(a == b);
-Console.WriteLine(a != b);
-
-
-
+// explicitly typed variables
+string word1 = "lololol";
+int num1 = 1;
 Console.ReadKey();

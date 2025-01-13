20f38f3472c947d8938099e180843ec4132c2d67
Julian Florez <forezjuli08@hotmail.com>
2025-01-12 19:26:20 -0500

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,33 @@
+﻿// .dll -> DLL = dynamic link library
+
+Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs!");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]remove a TODO");
+Console.WriteLine("[E]xit");
+
+
+string userInput = "A";
+
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number; // declaration
+number = 123; // initialization
+
+
+Console.WriteLine(number);
+number = 69;
+Console.WriteLine(number);
+
+// multiple declaration
+string name, lastName;
+// multiple declaration & initialization
+int num1 = 2, num2 = 7;
+
+string
+
+
+Console.ReadKey();

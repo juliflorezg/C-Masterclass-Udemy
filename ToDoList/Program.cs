912a091e34894b758c16fffed2e19b8505b68099
Julian Florez <forezjuli08@hotmail.com>
2025-01-20 22:13:34 -0500

    scope of local variables

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -12,9 +12,19 @@ Console.WriteLine("[E]xit");
 string userChoice = Console.ReadLine();
 Console.WriteLine("User input: " + userChoice);
 
-userChoice = "AEIOU";
+//userChoice = "AEIOU";
     
-var result = "abc" + "def" + "ghi";
+if (userChoice?.Length  == 0)
+{
+    Console.WriteLine("empty choice");
+    int number = 3;
+    Console.WriteLine(number);
+} else
+{
+    Console.WriteLine("non-empy value");
+    //Console.WriteLine(number); //! not valid, number variables is not present in this scope
+
+}
 
 
 Console.ReadKey(); 

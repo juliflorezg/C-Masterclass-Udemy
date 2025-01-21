3401f4c640af3b25663708bf544db603c232911d
Julian Florez <forezjuli08@hotmail.com>
2025-01-20 23:05:33 -0500

    methods 2: non-void methods

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -3,6 +3,7 @@
 
 // alt + drag with the mouse -> select multiple lines
 
+
 Console.WriteLine("Hello!");
 Console.WriteLine("[S]ee all TODOs!");
 Console.WriteLine("[A]dd a TODO");
@@ -11,6 +12,16 @@ Console.WriteLine("[E]xit");
 
 string userChoice = Console.ReadLine();
 Console.WriteLine("User input: " + userChoice);
+
+var result = Add(10, 5);
+Console.WriteLine("10 + 5 => " + result);
+
+var isLong = IsStringLong(userChoice);
+
+bool IsStringLong(string input)
+{
+    return input.Length > 10;
+}
 
 //userChoice = "AEIOU";
 
@@ -34,5 +45,10 @@ void PrintSelectedOption(string selectedOption) // parameteres
     Console.WriteLine("selected option:" + selectedOption);
 }
 
+int Add(int a, int b)
+{
+    return a + b;
+}
 
-Console.ReadKey();  
+
+Console.ReadKey()

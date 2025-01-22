2ffa311884289c9c9bcc5ca22dd1cb26cd246093
Julian Florez <forezjuli08@hotmail.com>
2025-01-22 10:06:04 -0500

    parsing string to int, exception

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -3,60 +3,66 @@
 
 // alt + drag with the mouse -> select multiple lines
 
+Console.WriteLine("please provide a number");
 
-Console.WriteLine("Hello!");
-Console.WriteLine("[S]ee all TODOs!");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]remove a TODO");
-Console.WriteLine("[E]xit");
-
-string userChoice = Console.ReadLine();
-Console.WriteLine("User input: " + userChoice);
-
-var result = Add(10, 5);
-Console.WriteLine("10 + 5 => " + result);
-
-var isLong = IsStringLong(userChoice);
-
-bool IsStringLong(string input)
-{
-    return input.Length > 10;
-}
-
-Console.WriteLine("abs sum:" + AbsoluteSum(10, 5));
-Console.WriteLine("abs sum: {0}", AbsoluteSum(-10, 5));
-
-//userChoice = "AEIOU";
-
-if (userChoice == "S")
-{
-    PrintSelectedOption("See all TODOs"); // arguments
-} else if (userChoice == "A")
-{ 
-    PrintSelectedOption("Add a TODO");
-} else if (userChoice == "R")
-{ 
-    PrintSelectedOption("Remove a TODO");
-} else if (userChoice == "E")
-{ 
-    PrintSelectedOption("Exit");
-}
-
-
-void PrintSelectedOption(string selectedOption) // parameteres
-{
-    Console.WriteLine("selected option:" + selectedOption);
-}
-
-int Add(int a, int b)
-{
-    return a + b;
-}
-
-int AbsoluteSum(int a, int b)
-{
-    return Math.Abs(a + b);
-}
-
-
+string input = Console.ReadLine();
+int number = int.Parse(input);
 Console.ReadKey();
+
+
+//Console.WriteLine("Hello!");
+//Console.WriteLine("[S]ee all TODOs!");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]remove a TODO");
+//Console.WriteLine("[E]xit");
+
+//string userChoice = Console.ReadLine();
+//Console.WriteLine("User input: " + userChoice);
+
+//var result = Add(10, 5);
+//Console.WriteLine("10 + 5 => " + result);
+
+//var isLong = IsStringLong(userChoice);
+
+//bool IsStringLong(string input)
+//{
+//    return input.Length > 10;
+//}
+
+//Console.WriteLine("abs sum:" + AbsoluteSum(10, 5));
+//Console.WriteLine("abs sum: {0}", AbsoluteSum(-10, 5));
+
+////userChoice = "AEIOU";
+
+//if (userChoice == "S")
+//{
+//    PrintSelectedOption("See all TODOs"); // arguments
+//} else if (userChoice == "A")
+//{ 
+//    PrintSelectedOption("Add a TODO");
+//} else if (userChoice == "R")
+//{ 
+//    PrintSelectedOption("Remove a TODO");
+//} else if (userChoice == "E")
+//{ 
+//    PrintSelectedOption("Exit");
+//}
+
+
+//void PrintSelectedOption(string selectedOption) // parameteres
+//{
+//    Console.WriteLine("selected option:" + selectedOption);
+//}
+
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
+
+//int AbsoluteSum(int a, int b)
+//{
+//    return Math.Abs(a + b);
+//}
+
+
+//Console.ReadKey();

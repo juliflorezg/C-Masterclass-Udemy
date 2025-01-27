0a0d967572440a9ff4a69e613b1562351879b704
Julian Florez <forezjuli08@hotmail.com>
2025-01-27 10:40:10 -0500

    add - CalculateSumOfNumbersBetween exercise

---
+++ CalculateSumOfNumbersBetween/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(CalculateSumOfNumbersBetween(5, 10));
+Console.WriteLine(CalculateSumOfNumbersBetween(-5, 5));
+Console.WriteLine(CalculateSumOfNumbersBetween(10, 10));
+
+
+
+static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+{
+    //your code goes here
+    if (lastNumber < firstNumber) return 0;
+    //if (firstNumber == lastNumber) return firstNumber;
+
+    int result = 0;
+
+    int counter = firstNumber;
+
+    while (counter <= lastNumber)
+    {
+        result += counter;
+        counter++;
+    }
+
+    //Console.WriteLine(result);
+    return result;
+
+}
+
+Console.ReadKey();

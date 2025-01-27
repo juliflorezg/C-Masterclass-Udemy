f686f6d92186d6b04e21947e3958458c7988a42d
Julian Florez <forezjuli08@hotmail.com>
2025-01-27 10:58:18 -0500

    add - RepeatCharacter exercise

---
+++ RepeatCharacter/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(RepeatCharacter('a', 4));
+Console.WriteLine(RepeatCharacter('!', 0));
+Console.WriteLine(RepeatCharacter('b', 1));
+Console.WriteLine(RepeatCharacter('c', -1));
+
+
+static string RepeatCharacter(char character, int targetLength)
+{
+    //your code goes here
+
+    string result = "";
+    do
+    {
+        result += character.ToString();
+    } while (result.Length < targetLength);
+
+    return result;
+}
+Console.ReadKey();

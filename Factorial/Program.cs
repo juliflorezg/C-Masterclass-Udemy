96829fc3900ce809caea3285d1ddc661933b75ea
Julian Florez <forezjuli08@hotmail.com>
2025-01-28 08:04:03 -0500

    add - factorial exercise

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Factorial(0));
+Console.WriteLine(Factorial(1));
+Console.WriteLine(Factorial(4));
+Console.WriteLine(Factorial(5));
+
+static int Factorial(int number)
+{
+    if (number == 0 || number == 1)
+    {
+        return 1;
+    }
+
+    int result = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+
+}
+
+Console.ReadKey();

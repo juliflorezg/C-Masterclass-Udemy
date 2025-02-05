af776ac55a61cc46668704c6512a675305e00d98
Julian Florez <forezjuli08@hotmail.com>
2025-02-04 22:02:20 -0500

    add - IsWordPresentInCollection exercise for arrays

---
+++ IsWordPresentInCollection/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Implement the IsWordPresentInCollection method, which
+ given an array of words and a wordToBeChecked, returns
+ true if wordToBeChecked is present in this collection and
+ false otherwise.
+ */
+
+static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+{
+
+    //foreach (string word in words) { 
+    //    if (wordToBeChecked.Contains(word)) return true; 
+    //}
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i] == wordToBeChecked)
+        {
+            return true;
+        }
+    }
+    return false;
+}
